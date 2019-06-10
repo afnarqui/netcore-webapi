60dc8f7919bf70217dc607d3f4fd02380205ace1
Andrés Felipe Naranjo Quintero <afnarqui9@gmail.com>
2019-06-10 09:44:02 -0500

    CU29: create docker-compose and deploy and aws

---
+++ Afnarqui.Prueba/Afnarqui.Prueba.Aplication.Logic/Travel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Afnarqui.Prueba.Aplication.Logic
+{
+    public class Travel
+    {
+
+        public string search()
+        {
+            
+            return "my string";
+        }
+       
+    }
+}
