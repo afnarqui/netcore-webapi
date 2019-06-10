25d18be80e1f53b1efa86048087dd0cf243c11d1
Andrés Felipe Naranjo Quintero <afnarqui9@gmail.com>
2019-06-09 19:42:32 -0500

    CU8 create interface ConnectionFactory in project Afnarqui.Prueba.Infraestructura.Data

---
+++ Afnarqui.Prueba/Afnarqui.Prueba.Infraestructura.Data/ConnectionFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Afnarqui.Prueba.Transversal.Common;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Afnarqui.Prueba.Infraestructura.Data
+{
+    public class ConnectionFactory: IConnectionFactory
+    {
+        private readonly IConfiguration iconfiguration;
+
+        public ConnectionFactory(IConfiguration configuration)
+        {
+            iconfiguration = configuration;
+        }
+
+        public IDbConnection GetConnection
+        {
+            get
+            {
+                var sqlConnection = new SqlConnection();
+                if (sqlConnection == null) return null;
+
+                sqlConnection.ConnectionString = iconfiguration.GetConnectionString("PruebaConnection");
+                sqlConnection.Open();
+                return sqlConnection;
+            }
+        }
+    }
+}
