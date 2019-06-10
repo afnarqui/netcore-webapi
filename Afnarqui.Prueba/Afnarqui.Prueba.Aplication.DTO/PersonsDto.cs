ad94823829ebacd01a8a02df983afca374ac518f
Andrés Felipe Naranjo Quintero <afnarqui9@gmail.com>
2019-06-09 22:35:17 -0500

    CU20 crear interface Persons in Afnarqui.Prueba.Aplication.DTO

---
+++ Afnarqui.Prueba/Afnarqui.Prueba.Aplication.DTO/PersonsDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Afnarqui.Prueba.Aplication.DTO
+{
+    public class PersonsDto
+    {
+        public int personsId { get; set; }
+        public int identity { get; set; }
+        public string password { get; set; }
+    }
+}
